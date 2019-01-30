e9d71124f754cfbfb9b0f4428bc4e5778acf5788
陈湘 <chenxiang2015@ipalfish.com>
2019-01-30 17:28:37 +0800

    船心版本

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
 const puppeteer = require('puppeteer');
 const superagent = require('superagent');
 const fs = require('fs');
+const ora = require('ora');
 const argv = require('optimist').argv;
-const UA = 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1';
-const timer = Date.now();
+const UA = 'Mozilla/5.0 (Linux; U; Android 8.1.0; zh-cn; BLA-AL00 Build/HUAWEIBLA-AL00) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/57.0.2987.132 MQQBrowser/8.9 Mobile Safari/537.36';
 (async () => {
   const browser = await puppeteer.launch({
     headless: false
   });
+  const spinner = ora('Loading unicorns').start()
   const page = await browser.newPage();
   await page.setUserAgent(UA);
   await page.goto(argv.url);
   await page.waitFor(1000);
   const bodyHandle1 = await page.$('body');
-  const videoURL = await page.evaluate(body => {
-    return body.querySelector('.video_iframe').src;
+  const iframeURL = await page.evaluate(body => {
+    return body.querySelector('iframe').src;
   }, bodyHandle1);
-  await page.goto(videoURL);
-  const bodyHandle2 = await page.$('body');
-  const videoSrc = await page.evaluate(body => body.querySelector('a').href, bodyHandle2);
+  await page.goto(iframeURL);
+  await page.waitFor(1000);
+  await page.click('a')
+  const videoSrc = page.url()
+  spinner.text = '下载比较慢，耐心等待'
+  const color = ['black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white', 'gray']
+  let timer = setInterval(() => {
+    spinner.color = color[Math.floor(Math.random() * 9)]
+  }, 200)
   superagent
     .get(videoSrc)
-    .end((err,res)=>{
-      fs.writeFile(`${__dirname}/video.mp4`,res.body,function(){
-        console.log('video has download')
-        process.exit()
+    .end((err, res) => {
+      fs.writeFile(`${__dirname}/video.mp4`, res.body, function () {
+        spinner.succeed('下完啦～～～')
+        clearInterval(timer)
+        setTimeout(() => {
+          process.exit()
+        }, 1000)
       });
     })
 })();
